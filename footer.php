669ca544a44051cac164341a22c8a24e139b3304
Hao Truong <haotrg035@gmail.com>
2019-10-06 19:31:05 +0700

    6/10/2019

--- footer.php
+++ footer.php
@@ -1,9 +1,6 @@
-<div class="w-100 py-4 bg-light"></div>
-<!-- Optional JavaScript -->
-<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-<script src="/lib/jquery/jquery-3.3.1.js"></script>
-<script src="/lib/bootstrap/js/bootstrap.bundle.js"></script>
-
-<script src="/asset/js/theme.js"></script>
-</body>
-</html>
+<script src="./lib/jquery/jquery-3.3.1.js"></script>
+<script src="./lib/sticky/jquery.sticky.js"></script>
+<script src="./lib/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="./asset/js/theme.js"></script>
+</body >
+</html >
--- index.php
+++ index.php
@@ -1,10 +1,153 @@
-<?php include './header.php' ?>
+<?php include "./header.php"; ?>
 
-<?php
-    $uri = ($_GET['site']);
-    if (file_exists('./' . $uri . '.php')) {
-        include './' . $uri . '.php';
-    }
-?>
+    <section id="page-hero">
+        <div class="jumbotron">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <h1 class="hero-header font-weight-bold">GIẢI PHÁP WEBSITE ƯU VIỆT</h1>
+                        <div class="hero-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi fugit
+                            laudantium
+                            provident rem ut?
+                            Dolore eligendi exercitationem minima minus officia perferendis rerum sapiente veritatis!
+                            Assumenda
+                            eos repudiandae sequi. Ab, ipsum.
+                        </div>
+                        <a class="btn btn-lg rounded-0 btn-hero-contact" href="#" role="button">
+                            TÌM HIỂU NGAY
+                        </a>
+                    </div>
+                    <div class="col-lg-5">
 
-<?php include './footer.php' ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a href="javascript:void(0)" id="btn-hero-scroll-down">
+            <i class="fa fa-angle-double-down" aria-hidden="true"></i>
+        </a>
+    </section>
+    <section class="home-section home-about py-4">
+        <div class="container">
+            <div class="row align-items-stretch">
+                <div class="col-12">
+                    <h2 class="text-center section-header">
+                        TẠI SAO LÀ <span class="color-base-text">WEBCORE</span>
+                    </h2>
+                    <div class="text-center section-header-text">
+                        <span class="color-base-text font-weight-bold">Webcore</span> ipsum dolor sit amet, consectetur
+                        adipisicing elit. Ab ad alias aliquam
+                        assumenda facilis in, laudantium magni nam possimus quam recusandae reprehenderit saepe sunt
+                        tempore ut! Ad cum numquam qui?
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="section-item">
+                        <h3 class="section-item-title text-center">Lorem olore eligendi</h3>
+                        <div class="section-item-content text-justify">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad alias aliquam
+                            assumenda facilis in, laudantium magni nam possimus quam recusandae reprehenderit saepe sunt
+                            tempore ut! Ad cum numquam qui? Lorem ipsum dolor sit amet
+                            recusandae reprehenderit saepe sunt
+                            tempore ut! Ad cum numquam qui? Lorem ipsum dolor sit amet
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="section-item">
+                        <h3 class="section-item-title text-center">Lorem olore eligendi</h3>
+                        <div class="section-item-content text-justify">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad alias aliquam
+                            assumenda facilis in, laudantium magni nam possimus quam recusandae reprehenderit saepe sunt
+                            tempore ut! Ad cum numquam qui? Lorem ipsum dolor sit amet recusandae reprehenderit saepe
+                            sunt
+                            tempore ut! Ad cum numquam qui? Lorem ipsum dolor sit amet
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="section-item">
+                        <h3 class="section-item-title text-center">Lorem olore eligendi</h3>
+                        <div class="section-item-content text-justify">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab ad alias aliquam
+                            assumenda facilis in, laudantium magni nam possimus quam recusandae reprehenderit saepe sunt
+                            tempore ut! Ad cum numquam qui? Lorem ipsum dolor sit amet
+                            Lorem ipsum dolor sit amet laudantium magni nam possimus quam
+                            possimus quam recusandae reprehenderit saepe sunt
+                            tempore ut! Ad cum numquam qui?
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="home-section home-service py-4">
+        <div class="container">
+            <h2 class="section-header text-center">
+                CÁC DỊCH VỤ <span class="color-base-text text-center">WEBCORE</span> CUNG CẤP
+            </h2>
+            <div class="section-content row">
+                <div class="col-lg-4">
+                    <div class="section-item">
+                        <figure class="section-item-top-icon">
+                            <i class="fa fa-desktop" aria-hidden="true"></i>
+                        </figure>
+                        <h5 class="section-item-title">THIẾT KẾ WEBSITE</h5>
+                        <div class="section-item-content text-justify">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, libero vero? Aspernatur
+                            ea earum facilis iure laborum molestiae nemo nobis officia praesentium quas quod quos
+                            recusandae, repellat reprehenderit voluptatem? Veniam!
+                        </div>
+                        <div class="section-item-footer">
+                            <a href="javascript:void(0)" class="btn btn-watch">
+                                <i class="fa fa-eye" aria-hidden="true"></i> XEM THÊM
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="section-item">
+                        <figure class="section-item-top-icon">
+                            <i class="fa fa-line-chart" aria-hidden="true"></i>
+                        </figure>
+                        <h5 class="section-item-title">ONLINE MARKETING</h5>
+                        <div class="section-item-content text-justify">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, libero vero? Aspernatur
+                            ea earum facilis iure laborum molestiae nemo nobis officia praesentium quas quod quos
+                            recusandae, repellat reprehenderit voluptatem? Veniam!
+                        </div>
+                        <div class="section-item-footer">
+                            <a href="javascript:void(0)" class="btn btn-watch">
+                                <i class="fa fa-eye" aria-hidden="true"></i> XEM THÊM
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="section-item">
+                        <figure class="section-item-top-icon">
+                            <i class="fa fa-server" aria-hidden="true"></i>
+                        </figure>
+                        <h5 class="section-item-title">HOSTING - SERVER</h5>
+                        <div class="section-item-content text-justify">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt, libero vero? Aspernatur
+                            ea earum facilis iure laborum molestiae nemo nobis officia praesentium quas quod quos
+                            recusandae, repellat reprehenderit voluptatem? Veniam!
+                        </div>
+                        <div class="section-item-footer">
+                            <a href="javascript:void(0)" class="btn btn-watch">
+                                <i class="fa fa-eye" aria-hidden="true"></i> XEM THÊM
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section style="height: 100vh">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem culpa delectus distinctio fugit magnam magni
+        molestias necessitatibus saepe suscipit voluptatum. Ab aperiam debitis dolorum error minus quos reiciendis
+        reprehenderit velit.
+    </section>
+<?php include "./footer.php"; ?>
